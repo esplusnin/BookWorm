f791da885e7affa001d6af8872bdb0dec74a236e
esplusnin <esplusnin94@yandex.ru>
2023-12-25 16:53:29 +0400

    №4 Добавить линтер SwiftLint

--- BookWorm/SupportingFiles/Localization/Strings+Generated.swift
+++ BookWorm/SupportingFiles/Localization/Strings+Generated.swift
@@ -10,6 +10,10 @@ import Foundation
 // swiftlint:disable explicit_type_interface function_parameter_count identifier_name line_length
 // swiftlint:disable nesting type_body_length type_name vertical_whitespace_opening_braces
 internal enum L10n {
+  /// hello
+  internal static let hello = L10n.tr("Localizable", "hello", fallback: "hello")
+  /// mamo
+  internal static let mamo = L10n.tr("Localizable", "mamo", fallback: "mamo")
 }
 // swiftlint:enable explicit_type_interface function_parameter_count identifier_name line_length
 // swiftlint:enable nesting type_body_length type_name vertical_whitespace_opening_braces
