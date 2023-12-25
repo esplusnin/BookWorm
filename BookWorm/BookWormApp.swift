accebd22da9f04e7ce1d4a02f68bac3e2829b277
esplusnin <esplusnin94@yandex.ru>
2023-12-25 13:56:52 +0400

    Initial Commit

---
+++ BookWorm/BookWormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookWormApp.swift
+//  BookWorm
+//
+//  Created by Евгений on 25.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct BookWormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
