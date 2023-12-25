e19d8dbb6db51fdc2ea7c3608e9fff9b63d63376
esplusnin <esplusnin94@yandex.ru>
2023-12-25 14:50:29 +0400

    №2 Добавить екстеншн на шрифты. Понизить таргет до 16.0

---
+++ BookWorm/Extensions/Font + Extension.swift
@@ -0,0 +1,19 @@
+//
+//  Fonts + Extension.swift
+//  BookWorm
+//
+//  Created by Евгений on 25.12.2023.
+//
+
+import SwiftUI
+
+extension Font {
+    static let largeTitleBoldFont = Font.system(size: 24, weight: .semibold)
+    static let mediumTitleBoldFont = Font.system(size: 20, weight: .semibold)
+    static let smallTitleBoldFont = Font.system(size: 16, weight: .semibold)
+    static let extraSmallTitleBoldFont = Font.system(size: 14, weight: .semibold)
+    
+    static let largeBodyFont = Font.system(size: 14, weight: .semibold)
+    static let mediumBodyBoldFont = Font.system(size: 10, weight: .medium)
+    static let mediumBodyFont = Font.system(size: 10, weight: .regular)
+}
